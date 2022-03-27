115109a7c37377320495a2aabbeb64ce82a15d61
Özgür Keleş <ozgur.keles@polirons.com>
2022-03-27 04:00:02 +0300

    Add RTE

---
+++ Rte.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "Bsw.h"
+#include "Rte.h"
+
+void Rte_Start(void)
+{
+    printf("RTE start\n");
+
+    Bsw_Start();
+}
---
+++ Rte.h
@@ -0,0 +1,14 @@
+#ifndef RTE_H
+#define RTE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void Rte_Start(void);
+
+#ifdef __cplusplus
+} /* extern C */
+#endif /* __cplusplus */
+
+#endif // RTE_H
